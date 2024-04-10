b82ad931f0a5a5146ecf9b3e018c411ef85fb423
abhishek bhadoriya <abhishek.bhadoriya@samarth.ac.in>
2024-04-10 15:21:31 +0530

    first commit

---
+++ src/userModule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace abhi\UserModule;
+
+use yii\base\Module;
+
+class UserModule extends Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'abhi\UserModule\Controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
+
+
